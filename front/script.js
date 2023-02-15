f02e91a953aaa19b371aee0d915165eda32382fb
Nikola Ubavic <53820106+ubavic@users.noreply.github.com>
2023-02-15 16:16:43 +0100

    fixes

--- front/script.js
+++ front/script.js
@@ -3,6 +3,17 @@ var points;
 
 var letters = ['У', 'Љ', 'А', 'В', 'Н', 'E', 'Б']
 
+const ranks =
+    [
+        {points: 0, rank: "Почетник"},
+        {points: 5, rank: "Добар"},
+        {points: 10, rank: "Солидан"},
+        {points: 20, rank: "Врло добар"},
+        {points: 30, rank: "Одличан"},
+        {points: 50, rank: "Изузетан"},
+        {points: 80, rank: "Геније"},
+    ]
+
 const renderWord = () => {
     const box = document.getElementById('line')
 
@@ -11,7 +22,7 @@ const renderWord = () => {
         .map(v => v !== letters[0] ? v : `<span class='theLetter'>${v}</span>`)
         .reduce((a, b) => a + b, "")
 
-    box.innerHTML = lettersHTML
+    box.innerHTML = lettersHTML + '<span id="blink"></span>'
 }
 
 const deleteWord = () => {
@@ -34,8 +45,11 @@ const shake = () => {
 }
 
 const checkWord = () => {
-    if (word === "") return
-    
+    if (word === "") return shake()
+
+    const nl = word.split('').filter(l => l == letters[0])
+    if (nl === 0) return shake()
+
     return fetch('/words', {
         headers: {
             'Accept': 'application/json',
@@ -48,13 +62,13 @@ const checkWord = () => {
 
         throw Error()
     }).then(r => {
-        if (r.Found = "true") {
-            points = 10 * word.length
+        if (r.Found === "true") {
+            points = points + 10 * word.length
             renderPoints()
             word = ""
             renderWord()
         } else {
-            shake()
+            return shake()
         }
     }).catch(r => {})
 }
@@ -64,6 +78,26 @@ const setLetters = () => {
         const hex = document.getElementById(`h${i}`)
         hex.children[1].textContent = letters[i]
     }
+}
+
+const shuffleWord = () => {
+    for (i = 1; i < 7; i++) {
+        const hex = document.getElementById(`h${i}`)
+        hex.children[1].classList.add('faded-text')
+    }
+
+    setTimeout(() => {
+        const xs = letters.slice(1).map(l => ({l, n: Math.random()})).sort((a, b) => a.n - b.n > 0).map(s => s.l)
+        letters = [letters[0], ...xs]
+        setLetters()
+    }, 200)
+
+    setTimeout(() => {
+        for (i = 1; i < 7; i++) {
+            const hex = document.getElementById(`h${i}`)
+            hex.children[1].classList.remove('faded-text')
+        }
+    }, 400)
 }
 
 const setup = () => {
